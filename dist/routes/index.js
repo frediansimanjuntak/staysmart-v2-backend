3e68cbfbe707fbb81401af9942703ddbef333107
Felix Prawira Azali <felixprawira8@gmail.com>
2017-02-21 09:36:49 +0700

    CRUD blog category

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,12 +1,14 @@
 "use strict";
 var todo_route_1 = require("../api/todo/route/todo-route");
 var users_routes_1 = require("../api/users/routes/users-routes");
+var blog_categories_routes_1 = require("../api/blog_categories/routes/blog_categories-routes");
 var Routes = (function () {
     function Routes() {
     }
     Routes.init = function (app, router) {
         todo_route_1.TodoRoutes.init(router);
         users_routes_1.UserRoutes.init(router);
+        blog_categories_routes_1.BlogCategoriesRoutes.init(router);
         app.use("/", router);
     };
     return Routes;
---
+++ server/api/blog_categories/controller/blog_categories-controller.ts
@@ -0,0 +1,46 @@
+import * as express from 'express';
+import BlogCategoriesDAO from '../dao/blog_categories-dao';
+var passport = require('passport');
+
+export class BlogCategoriesController {
+	static getAll(req: express.Request, res: express.Response):void {
+		BlogCategoriesDAO
+		['getAll']()
+		.then(blog_categories => res.status(200).json(blog_categories))
+		.catch(error => res.status(400).json(error));
+	}
+
+	static getById(req: express.Request, res: express.Response):void {
+		let _id = req.params.id;
+		BlogCategoriesDAO
+		['getById'](_id)
+		.then(blog_categories => res.status(200).json(blog_categories))
+		.catch(error => res.status(400).json(error));
+	}
+
+	static createBlogCategories(req: express.Request, res: express.Response):void {
+		let _blog_categories = req.body;
+		BlogCategoriesDAO
+		['createBlogCategories'](_blog_categories)
+		.then(blog_categories => res.status(201).json(blog_categories))
+		.catch(error => res.status(400).json(error));
+	}
+
+	static deleteBlogCategories(req: express.Request, res: express.Response):void {
+		let _id = req.params.id;
+		BlogCategoriesDAO
+		['deleteBlogCategories'](_id)
+		.then(() => res.status(200).end())
+		.catch(error => res.status(400).json(error));
+	}
+
+	static updateBlogCategories(req: express.Request, res: express.Response):void {
+		let _id = req.params.id;
+		let _blog_categories = req.body;
+
+		BlogCategoriesDAO
+		['updateBlogCategories'](_id, _blog_categories)
+		.then(blog_categories => res.status(201).json(blog_categories))
+		.catch(error => res.status(400).json(error));
+	}
+}
