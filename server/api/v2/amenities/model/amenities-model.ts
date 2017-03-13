79699378e4018801235e868afef329ee08f3bb3f
Felix Prawira Azali <felixprawira8@gmail.com>
2017-03-13 16:32:31 +0700

    add icon to amenities model, update delete amenities function

--- server/api/v2/amenities/model/amenities-model.ts
+++ server/api/v2/amenities/model/amenities-model.ts
@@ -10,6 +10,10 @@ var Schema = mongoose.Schema;
 var AmenitiesSchema = new mongoose.Schema({
 	name: {type: String},
 	description: {type: String},
+	icon: {
+		type: Schema.Types.ObjectId,
+		ref: 'Attachments'
+	},
 	created_at: {type: Date, default: Date.now}
 });
 
