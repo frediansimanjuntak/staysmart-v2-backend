6212dcd15392dff10db7a3f80f85fb6476440acf
Felix Prawira Azali <felixprawira8@gmail.com>
2017-05-31 16:02:19 +0700

    add helper for /me

--- server/api/v2/users/dao/users-dao.ts
+++ server/api/v2/users/dao/users-dao.ts
@@ -17,7 +17,7 @@ import config from '../../../../config/environment/index';
 import {GlobalService} from '../../../../global/global.service';
 import {socketIo} from '../../../../server';
 var split = require('split-string');
-
+import {userHelper} from '../../../../helper/user.helper';
 usersSchema.static('index', ():Promise<any> => {
 	return new Promise((resolve:Function, reject:Function) => {
 		Users
@@ -150,7 +150,7 @@ usersSchema.static('getUser', (query:Object):Promise<any> => {
 					}
 				}]	
 			})
-	        .populate({
+			.populate({
 				path: 'shortlisted_properties',
 				populate: [{
 					path: 'development',
@@ -267,108 +267,11 @@ usersSchema.static('getAll', ():Promise<any> => {
 usersSchema.static('me', (userId:string, headers:Object):Promise<any> => {
 	return new Promise((resolve:Function, reject:Function) => {
 		let _query = {"_id": userId};
-		let header: any = headers;
 		Users.getUser(_query).then(res => {
 			_.each(res, (result) => {
-				if (header.from && header.from == 'Mobile') {
-					let auth = header.authorization;
-					let auth_code = auth.slice(7);
-					let front_landlord;
-					let back_landlord;
-					let front_tenant;
-					let back_tenant;
-					if (result.landlord.data.identification_proof.front){
-						front_landlord = result.landlord.data.identification_proof.front.url	
-					}
-					else {
-						front_landlord = null;
-					}
-					if (result.landlord.data.identification_proof.back != null) {
-						let back_landlord = result.landlord.data.identification_proof.back.url;
-					}
-					else {
-						let back_landlord = null;
-					}
-					if (result.tenant.data.identification_proof.front){
-						front_tenant = result.tenant.data.identification_proof.front.url	
-					}
-					else {
-						front_tenant = null;
-					}
-					if (result.tenant.data.identification_proof.back != null) {
-						let back_tenant = result.tenant.data.identification_proof.back.url;
-					}
-					else {
-						let back_tenant = null;
-					}
-					let type;
-					if (result.companies.length > 0) {
-						type = 'company';
-					}
-					else {
-						type = 'individu';
-					}
-					let status = result.verification.verified;
-					if (status == true) {
-						status = 'verified';
-					}
-					else {
-						status = 'unverified';
-					}
-					let landlord_data = {
-						full_name: result.landlord.data.name,
-						type: result.landlord.data.identification_type,
-						id_number: result.landlord.data.identification_number,
-						user: result._id,
-						identity_front: front_landlord,
-						identity_back: back_landlord,
-						owner: result.landlord.data.owners,
-						type_landlord: type
-					};
-					let tenant_data = {
-						user: result._id,
-						phone: result.phone,
-						confirmation_status: status,
-						data: {
-							identity_back: back_tenant,
-							identity_front: front_tenant,
-							name: result.tenant.data.name,
-							id_no: result.tenant.data.identification_number,
-							type: result.tenant.data.identification_type
-						}
-					};
-					Managers
-						.find({"manager": result._id, "status": "pending"}, '-_id property')
-						.exec((err, res) => {
-							if (err) {
-								reject({message: err.message});
-							}
-							else {
-								resolve({
-									authorization: auth_code,
-									_id: result._id,
-									profil: {
-										forgot: {
-											code: result.reset_password.token,
-											expire: result.reset_password.expired_at
-										}
-									},
-									username: result.username,
-									email: result.email,
-									roles: result.role,
-									landlord: landlord_data,
-									tenant: tenant_data,
-									picture: result.picture.url,
-									owned_property: result.owned_properties,
-									managed_property: result.managed_properties,
-									appointed_property: res
-								});
-							}
-						})
-				}
-				else {
-					resolve(result);	
-				}
+				userHelper.meHelper(result, headers).then(res_data => {
+					resolve(res_data);
+				});
 			})	
 		})
 		.catch((err) => {
