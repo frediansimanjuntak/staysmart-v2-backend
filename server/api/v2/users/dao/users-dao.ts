9f14c396a9477fd7c52c6a22bbb967934d0321c5
Felix Prawira <felixprawira8@gmail.com>
2017-06-19 14:37:53 +0700

    merge data user with facebook if user already exist

--- server/api/v2/users/dao/users-dao.ts
+++ server/api/v2/users/dao/users-dao.ts
@@ -1698,14 +1698,16 @@ usersSchema.static('facebookLoginMobile', (data: Object, device: string):Promise
 					Users.find({"service.facebook.id": res.id}).exec((err, result) => {
 						if (err) { reject({message: err.message}); }
 						else if (result.length == 0) {
+							let email;
 							var _new_user = new Users();
 								_new_user.username = res.id;
 								if (body.email) {
-									_new_user.email = body.email;
+									email = body.email;
 								}
 								else {
-									_new_user.email = res.email;
+									email = res.email;
 								}
+								_new_user.email = email;
 								_new_user.service.facebook.id = res.id;
 								_new_user.service.facebook.token = body.accessToken;
 								_new_user.service.facebook.picture = "http://graph.facebook.com/"+res.id+"/picture/?type=large";
@@ -1713,30 +1715,31 @@ usersSchema.static('facebookLoginMobile', (data: Object, device: string):Promise
 									if (err) { 
 										if (device == 'desktop') { reject(err); }
 										else {
-											let error = [];
-											err.errors.username ? error.push('username') : '';
-											err.errors.email ? error.push('email') : '';
-											err.errors.phone ? error.push('phone') : '';
-											err.errors.password ? error.push('password'): '';
-
-											let message;
-											if (error.length > 2) {
-												let error_message = '';
-												for (var i = 0; i < error.length - 1; i++){
-													error_message = error_message + error[i] + ', ';
-												}
-												message = 'Incorrect ' + error_message + 'or ' + error[error.length-1];
+											if (err.errors.email) {
+												Users.findOne({"email": email}).exec((err, user) => {
+													if (err) { reject({message: err.message}); }
+													else {
+														user.service.facebook.id = res.id;
+														user.service.facebook.token = body.accessToken;
+														user.service.facebook.picture = "http://graph.facebook.com/"+res.id+"/picture/?type=large";
+														user.save((err, userSaved) => {
+															if (err) { reject({message: err.message}); }
+															else {
+																let token = signToken(userSaved._id, userSaved.role, userSaved.username);
+																resolve({
+																	"x-auth-token": token,
+																	_id: userSaved._id,
+																	email: userSaved._id,
+																	roles: userSaved.role,
+																	verified: userSaved.verification.verified,
+																	picture: userSaved.service.facebook.picture
+																});
+															}
+														})
+													}
+												})
 											}
-											else if (error.length == 2) {
-												message = 'Incorrect ' + error[0] + ' or ' + error[1];
-											}
-											else {
-												message = error[0] + ' null';
-											}
-											reject({
-												message: message,
-												code: 400
-											});
+											
 										}
 									}
 									else {
